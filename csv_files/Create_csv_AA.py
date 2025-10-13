2892b126c42efbd88deaad4bb89dec4cb4f810f6
Jakob Juul Hansen <jakobjuulhansen@gmail.com>
2025-10-13 10:22:09 +0200

    Changed it back again, sorry

--- csv_files/Create_csv_AA.py
+++ csv_files/Create_csv_AA.py
@@ -11,4 +11,17 @@ sheets_to_export = ["EXPORT US USD", "EXPORT US EUR", "EXPORT EU EUR"]
 for sheet in sheets_to_export:
     df = pd.read_excel(excel_file, sheet_name=sheet)
     df.to_csv(f"csv_files/long_{sheet}.csv", index=False)
+    print(f"Saved long_{sheet}.csv")
+
+
+# Path to your Excel file
+excel_file = "csv_files/factor_regression_data.xlsx"
+
+# List of sheet names (arcs) you want to export
+sheets_to_export = ["EXPORT US USD", "EXPORT US EUR", "EXPORT EU EUR"]
+
+# Loop through and save each as CSV
+for sheet in sheets_to_export:
+    df = pd.read_excel(excel_file, sheet_name=sheet)
+    df.to_csv(f"csv_files/{sheet}.csv", index=False)
     print(f"Saved {sheet}.csv")

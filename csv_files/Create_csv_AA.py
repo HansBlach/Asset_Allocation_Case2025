eb3aca6e42cc47d9310b4892684be39da94e433c
Jakob Juul Hansen <jakobjuulhansen@gmail.com>
2025-10-13 09:45:02 +0200

    Converted everything to decimals

--- csv_files/Create_csv_AA.py
+++ csv_files/Create_csv_AA.py
@@ -2,7 +2,7 @@
 import pandas as pd
 
 # Path to your Excel file
-excel_file = "csv_files/long_factor_regression_data.xlsx"
+excel_file = "csv_files/factor_regression_data.xlsx"
 
 # List of sheet names (arcs) you want to export
 sheets_to_export = ["EXPORT US USD", "EXPORT US EUR", "EXPORT EU EUR"]
@@ -10,5 +10,5 @@ sheets_to_export = ["EXPORT US USD", "EXPORT US EUR", "EXPORT EU EUR"]
 # Loop through and save each as CSV
 for sheet in sheets_to_export:
     df = pd.read_excel(excel_file, sheet_name=sheet)
-    df.to_csv(f"csv_files/long_{sheet}.csv", index=False)
+    df.to_csv(f"csv_files/{sheet}.csv", index=False)
     print(f"Saved {sheet}.csv")
